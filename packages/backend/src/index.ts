c9ba9d42d4114edcfc45382a99d108d250f0e46b
mashkovd <mashkovd@live.ru>
2026-02-14 02:07:06 +0100

    feat: add http-request scaffolder action for Vault integration

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,6 +16,7 @@ backend.add(import('@backstage/plugin-proxy-backend'));
 // scaffolder plugin
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@roadiehq/scaffolder-backend-module-http-request'));
 
 // techdocs plugin
 backend.add(import('@backstage/plugin-techdocs-backend'));
